dcf34bf28f7159d103bdccff6cb2687a6fc6d57b
Christof Schulze <christof.schulze@pingtech.de>
2022-05-29 12:37:21 +0200

    entrypoint: allow setting JAVA_ARGS
    Order does matter for java CLI options. Therefore we allow setting java arguments before calling the jar file leaving EXTRA_ARGS to be set as parameter to openfire itself

--- entrypoint.sh
+++ entrypoint.sh
@@ -63,7 +63,7 @@ initialize_log_dir
 # default behaviour is to launch openfire
 if [[ -z ${1} ]]; then
   exec start-stop-daemon --start --chuid ${OPENFIRE_USER}:${OPENFIRE_USER} --exec /usr/bin/java -- \
-    -Dlog4j.configurationFile=${OPENFIRE_DATA_DIR}/conf/log4j2.xml \
+    ${JAVA_ARGS} -Dlog4j.configurationFile=${OPENFIRE_DATA_DIR}/conf/log4j2.xml \
     -DopenfireHome=/usr/share/openfire \
     -Dopenfire.lib.dir=/usr/share/openfire/lib \
     -classpath /usr/share/openfire/lib/startup.jar \
